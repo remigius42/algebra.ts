76c08bbc05f28ac06842606ebd83ba1e824200aa
Andreas Remigius Schmidt <remigius42-github.dcds7@aleeas.com>
2024-03-01 10:17:27 +0100

    build: transpile build with Babel
    Transpile the new Rollup build with Babel to ensure widespread
    compatibility.
    
    - Add necessary Babel NPM dev dependencies including core-js
    - Configure target environments with `.browserslistrc`
    - Enable Babel transpilation in the Rollup configuration

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,6 @@
+import { babel } from "@rollup/plugin-babel"
 import commonjs from "@rollup/plugin-commonjs"
+import { nodeResolve } from "@rollup/plugin-node-resolve"
 import terser from "@rollup/plugin-terser"
 import pkg from "./package.json" assert { type: "json" }
 
@@ -13,6 +15,11 @@ export default compact.flatMap(compact =>
       name: "algebra",
       format
     },
-    plugins: [commonjs(), compact ? terser() : undefined]
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+      babel({ babelHelpers: "bundled", exclude: [/core-js/] }),
+      compact ? terser() : undefined
+    ]
   }))
 )
